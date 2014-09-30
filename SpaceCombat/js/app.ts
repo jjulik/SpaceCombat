9a1a6ac926c990bb78c8e6aaa898f950e4f3279b
jjulik <jjulik@outlook.com>
2014-09-30 01:27:59 -0500

    Got the fighter moving back and forth with keyboard input

--- SpaceCombat/js/app.ts
+++ SpaceCombat/js/app.ts
@@ -1,42 +1,83 @@
 ﻿///<reference path="lib/PIXI.d.ts"/>
-class SpaceCombatEngine {
-    renderer: PIXI.WebGLRenderer;
-    stage: PIXI.Stage;
-    texture: PIXI.Texture;
-    fighter: PIXI.Sprite;
-    canvasWidth: number;
-    canvasHeight: number;
-
-    constructor() {
-        this.canvasWidth = window.innerWidth;
-        this.canvasHeight = window.innerHeight;
-        this.renderer = new PIXI.WebGLRenderer(this.canvasWidth, this.canvasHeight);
-        this.stage = new PIXI.Stage(0x000000);
-        this.texture = PIXI.Texture.fromImage('img/fighter.png');
-        this.fighter = new PIXI.Sprite(this.texture);
-
-        // add the renderer view element to the DOM
-        document.body.appendChild(this.renderer.view);
-
-        this.fighter.anchor.x = 0.5;
-        this.fighter.anchor.y = 0.5;
-        this.fighter.position.x = 400;
-        this.fighter.position.y = 300;
-
-        this.stage.addChild(this.fighter);
+module SpaceCombat {
+    enum KeyCode {
+        LEFT = 37,
+        UP = 38,
+        RIGHT = 39,
+        DOWN = 40
     }
 
-    public animate(): void {
-        requestAnimationFrame(() => this.animate());
+    class SpaceCombatEngine {
+        renderer: PIXI.WebGLRenderer;
+        stage: PIXI.Stage;
+        texture: PIXI.Texture;
+        fighter: PIXI.Sprite;
+        canvasWidth: number;
+        canvasHeight: number;
+        pressedKeys: Array<boolean>;
 
-        this.fighter.rotation += 0.1;
+        get speedModifer(): number {
+            return 10;
+        }
 
-        this.renderer.render(this.stage);
+        constructor() {
+            this.canvasWidth = document.body.clientWidth;
+            this.canvasHeight = document.body.clientHeight;
+            this.renderer = new PIXI.WebGLRenderer(this.canvasWidth, this.canvasHeight);
+            this.stage = new PIXI.Stage(0x000000);
+            this.texture = PIXI.Texture.fromImage('img/fighter.png');
+            this.fighter = new PIXI.Sprite(this.texture);
+
+            // add the renderer view element to the DOM
+            document.body.appendChild(this.renderer.view);
+
+            this.fighter.anchor.x = 0.5;
+            this.fighter.anchor.y = 0.5;
+            this.fighter.rotation = -Math.PI / 2;
+            this.fighter.position.x = this.canvasWidth / 2;
+            this.fighter.position.y = this.canvasHeight - 50;
+            this.stage.addChild(this.fighter);
+
+            this.pressedKeys = [];
+            this.registerKeyHandlers();
+        }
+
+        registerKeyHandlers() {
+            document.addEventListener('keydown', (event) => {
+                //should we even bother with a switch case?
+                switch (event.keyCode) {
+                    case KeyCode.LEFT:
+                    case KeyCode.UP:
+                    case KeyCode.RIGHT:
+                    case KeyCode.DOWN:
+                        this.pressedKeys[event.keyCode] = true;
+                        break;
+                }
+            });
+        }
+
+        processKeyboardInput() {
+            if (this.pressedKeys[KeyCode.LEFT] && this.fighter.position.x > 0) {
+                this.fighter.position.x -= this.speedModifer;
+            }
+            if (this.pressedKeys[KeyCode.RIGHT] && this.fighter.position.x < this.canvasWidth) {
+                this.fighter.position.x += this.speedModifer;
+            }
+            this.pressedKeys = [];
+        }
+
+        animate() {
+            requestAnimationFrame(() => this.animate());
+
+            this.processKeyboardInput();
+
+            this.renderer.render(this.stage);
+        }
     }
+
+    window.onload = () => {
+        var sce: SpaceCombatEngine;
+        sce = new SpaceCombatEngine();
+        sce.animate();
+    };
 }
-
-window.onload = () => {
-    var sce: SpaceCombatEngine;
-    sce = new SpaceCombatEngine();
-    sce.animate();
-};
