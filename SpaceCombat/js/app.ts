b170f31614b11104b133d88273bd5005d9c6152e
jjulik <jjulik@outlook.com>
2014-10-01 22:18:35 -0500

    Moved Fighter into the Character module

--- SpaceCombat/js/app.ts
+++ SpaceCombat/js/app.ts
@@ -1,21 +1,12 @@
 ﻿///<reference path="lib/PIXI.d.ts"/>
+///<reference path="character/Fighter.ts"/>
 module SpaceCombat {
-    enum KeyCode {
-        LEFT = 37,
-        UP = 38,
-        RIGHT = 39,
-        DOWN = 40
-    }
-
     class SpaceCombatEngine {
         renderer: PIXI.WebGLRenderer;
         stage: PIXI.Stage;
-        texture: PIXI.Texture;
-        fighter: PIXI.Sprite;
+        fighter: Character.Fighter;
         canvasWidth: number;
         canvasHeight: number;
-        minY: number;
-        maxY: number;
         pressedKeys: Array<boolean>;
 
         get xSpeedModifier(): number {
@@ -29,24 +20,15 @@ module SpaceCombat {
         constructor() {
             this.canvasWidth = document.body.clientWidth;
             this.canvasHeight = document.body.clientHeight;
-            // the lowest the fighter can go on the screen
-            this.minY = this.canvasHeight - 50;
-            // the highest the fighter can go on the screen
-            this.maxY = 50;//Math.max(this.minY - 300, 50);
             this.renderer = new PIXI.WebGLRenderer(this.canvasWidth, this.canvasHeight);
             this.stage = new PIXI.Stage(0x000000);
-            this.texture = PIXI.Texture.fromImage('img/fighter.png');
-            this.fighter = new PIXI.Sprite(this.texture);
+            
+            this.fighter = new Character.Fighter(this.canvasWidth, this.canvasHeight);
 
             // add the renderer view element to the DOM
             document.body.appendChild(this.renderer.view);
 
-            this.fighter.anchor.x = 0.5;
-            this.fighter.anchor.y = 0.5;
-            this.fighter.rotation = -Math.PI / 2;
-            this.fighter.position.x = this.canvasWidth / 2;
-            this.fighter.position.y = this.minY;
-            this.stage.addChild(this.fighter);
+            this.stage.addChild(this.fighter.sprite);
 
             this.pressedKeys = [];
             this.registerKeyHandlers();
@@ -62,19 +44,7 @@ module SpaceCombat {
         }
 
         processKeyboardInput() {
-            if (this.pressedKeys[KeyCode.LEFT] && this.fighter.position.x > 0) {
-                this.fighter.position.x -= this.xSpeedModifier;
-            }
-            if (this.pressedKeys[KeyCode.RIGHT] && this.fighter.position.x < this.canvasWidth) {
-                this.fighter.position.x += this.xSpeedModifier;
-            }
-            if (this.pressedKeys[KeyCode.DOWN] && this.fighter.position.y < this.minY) {
-                this.fighter.position.y += this.ySpeedModifier;
-            }
-            if (this.pressedKeys[KeyCode.UP] && this.fighter.position.y > this.maxY) {
-                this.fighter.position.y -= this.ySpeedModifier;
-            }
-
+            this.fighter.processInput(this.pressedKeys);
         }
 
         animate() {
---
+++ SpaceCombat/js/character/Fighter.ts
@@ -0,0 +1,59 @@
+﻿///<reference path="../lib/PIXI.d.ts"/>
+module SpaceCombat.Character {
+    enum KeyCode {
+        LEFT = 37,
+        UP = 38,
+        RIGHT = 39,
+        DOWN = 40
+    }
+
+    export class Fighter {
+        canvasHeight: number;
+        canvasWidth: number;
+        minY: number;
+        maxY: number;
+        sprite: PIXI.Sprite;
+        texture: PIXI.Texture;
+
+        get xSpeedModifier(): number {
+            return 20;
+        }
+
+        get ySpeedModifier(): number {
+            return 20;
+        }
+
+        constructor(canvasWidth: number, canvasHeight: number) {
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
+            // the lowest the fighter can go on the screen
+            this.minY = this.canvasHeight - 50;
+            // the highest the fighter can go on the screen
+            this.maxY = 50;//Math.max(this.minY - 300, 50);
+
+            this.texture = PIXI.Texture.fromImage('img/fighter.png');
+            this.sprite = new PIXI.Sprite(this.texture);
+
+            this.sprite.anchor.x = 0.5;
+            this.sprite.anchor.y = 0.5;
+            this.sprite.rotation = -Math.PI / 2;
+            this.sprite.position.x = this.canvasWidth / 2;
+            this.sprite.position.y = this.minY;
+        }
+
+        processInput(pressedKeys) {
+            if (pressedKeys[KeyCode.LEFT] && this.sprite.position.x > 0) {
+                this.sprite.position.x -= this.xSpeedModifier;
+            }
+            if (pressedKeys[KeyCode.RIGHT] && this.sprite.position.x < this.canvasWidth) {
+                this.sprite.position.x += this.xSpeedModifier;
+            }
+            if (pressedKeys[KeyCode.DOWN] && this.sprite.position.y < this.minY) {
+                this.sprite.position.y += this.ySpeedModifier;
+            }
+            if (pressedKeys[KeyCode.UP] && this.sprite.position.y > this.maxY) {
+                this.sprite.position.y -= this.ySpeedModifier;
+            }
+        }
+    }
+} 
